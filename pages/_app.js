6740a0fdafb0c35a465744b0611d11ec3d235396
Usman Murtala <uzlash16@gmail.com>
2022-07-30 02:32:12 +0100

    added navbar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+// import { NFTProvider } from "../context/NFTContext";
+import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  // <NFTProvider>
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+
+    <Script
+      src="https://kit.fontawesome.com/d7cc51c498.js"
+      crossOrigin="anonymous"
+    />
+  </ThemeProvider>
+  // </NFTProvider>
+);
 
 export default MyApp;
